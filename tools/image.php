fd08db8418a8e189d892a25b9a1536eade2860ef
Physics Police <physics.police@gmail.com>
2016-05-21 19:26:14 -0700

    Changed image dimensions.

--- tools/image.php
+++ tools/image.php
@@ -3,7 +3,7 @@
 function image_init()
 {
 	global $image, $white, $black, $empty, $count;
-	$image = imagecreatetruecolor(1024, 8 * 32);
+	$image = imagecreatetruecolor(512, 13 * 32);
 	$white = imagecolorallocate($image, 255, 255, 255);
 	$black = imagecolorallocate($image,   0,   0,   0);
 	$empty = imagecolorallocate($image, 128, 128, 128);
@@ -37,8 +37,8 @@ function image_next_location()
 	global $count;
 	$y = $count % 32;
 	$x = floor($count / 32);
-	$y += floor($x / 1024) * 48;
-	$x %= 1024;
+	$y += floor($x / 512) * 48;
+	$x %= 512;
 	$count++;
 	return array($x, $y);
 }
